1f875a4c3f9db9e1ee6c8ca3b4eb8885701ecd94
Yuziem14 <yuri.fnunes@gmail.com>
2019-11-19 06:12:03 -0300

    Crating Layout

--- start/routes.js
+++ start/routes.js
@@ -19,4 +19,4 @@ require('./authRoutes.js');
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('index')
