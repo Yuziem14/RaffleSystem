4330d76c8b91921b922108dd876c16d9762a89d8
yuziem14 <yuri.fnunes@gmail.com>
2019-11-21 18:26:18 -0300

    Updating Views #2

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -3,7 +3,11 @@ const Raffle = use('App/Models/Raffle');
 
 class HomeController {
     async index({ view }) {
-        const raffles = (await Raffle.all()).rows
+        const raffles = (await Raffle.all()).rows;
+        raffles.forEach( async raffle => {
+            raffle.total = await raffle.tickets().getCount();
+            raffle.availables = await raffle.tickets().where('user_id', null).getCount();
+        });
         return view.render('index', { raffles })
     }
 }
